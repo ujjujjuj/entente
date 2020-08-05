47008fa9fdb7468db4b7cd8d65a2109893361183
M4ngo3ater <ujjwaldimri@hotmail.com>
2020-08-05 12:10:36 +0530

    removed https

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 app.use(express.static(__dirname + '/public'));
-app.use(sslRedirect());
+//app.use(sslRedirect());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload())
 app.use(cookieParser());
